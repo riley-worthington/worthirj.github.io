b403a4eff3d76a5f3c72ddccbb805c7e4e9d89ae
Riley Worthington <worthirj@whitman.edu>
2018-08-27 14:55:34 -0700

    Add content and styling

---
+++ scripts/portfolio.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({
+    reset: false,
+    duration: 600,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    scale: 1,
+    viewFactor: 0.3,
+  });
+
+sr.reveal('.portfolio-item');
+sr.reveal('.resume-box');
+sr.reveal('footer');
